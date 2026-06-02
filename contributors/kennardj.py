6d75fcc6884a9aa92b9556471676336710e65b76
Jasmin Kennard <jjk353@cornell.edu>
2026-06-02 09:57:37 -0700

    Modified file to include introduce function

--- contributors/kennardj.py
+++ contributors/kennardj.py
@@ -1,1 +1,9 @@
-print('Jasmin Kennard')
+# Contributor: Jasmin Kennard
+# ONID:        kennardj
+# Term:        Spring 2026
+
+def introduce():
+    print("Hi, I'm Jasmin Kennard, ONID kennardj.")
+
+if __name__ == "__main__":
+    introduce()
